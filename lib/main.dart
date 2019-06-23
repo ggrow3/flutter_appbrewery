1a0d18c160f92584b1d2785abfe60139b4e80dcd
Kevin MacNeel <kevjac@gmail.com>
2019-06-22 22:15:52 -0700

    Added image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber[50],
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
+
       ),
-    ));
+    ),
+  );
+}
