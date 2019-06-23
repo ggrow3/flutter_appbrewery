d1e0369c68c420802c836a8b67b4afd57518479f
Kevin MacNeel <kevjac@gmail.com>
2019-06-22 21:29:27 -0700

    Center Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp( home:Text("Hello World"));
-
+void main() => runApp(MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ));
